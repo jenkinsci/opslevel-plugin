c95f32b7bedee1293bcf4c9f58171cb3b2d83d72
Sergio <sergio@opslevel.com>
2021-05-18 16:49:24 -0400

    Initial commit

---
+++ src/main/java/io/jenkins/plugins/NotificationEvent.java
@@ -0,0 +1,17 @@
+package io.jenkins.plugins;
+
+public class NotificationEvent {
+    public NotificationEvent(String projectName, String buildName, String buildUrl, String buildVars, String event) {
+        this.projectName = projectName;
+        this.buildName = buildName;
+        this.buildUrl = buildUrl;
+        this.buildVars = buildVars;
+        this.event = event;
+    }
+
+    public String projectName;
+    public String buildName;
+    public String buildUrl;
+    public String buildVars;
+    public String event;
+}
