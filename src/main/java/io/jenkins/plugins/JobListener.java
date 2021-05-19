5b07e42e36fc415df9bdeada46dd320bcfd9fe36
Sergio <sergio@opslevel.com>
2021-05-19 11:06:33 -0400

    Fix and break more things

---
+++ src/main/java/io/jenkins/plugins/JobListener.java
@@ -0,0 +1,95 @@
+package io.jenkins.plugins;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.Result;
+import hudson.model.TaskListener;
+import hudson.model.listeners.RunListener;
+import com.alibaba.fastjson.JSON;
+import okhttp3.*;
+
+import javax.annotation.Nonnull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Extension
+public class JobListener extends RunListener<AbstractBuild> {
+
+    private static final MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient client;
+
+    private static final Logger log = LoggerFactory.getLogger(JobListener.class);
+
+    public JobListener() {
+        super(AbstractBuild.class);
+        client = new OkHttpClient();
+    }
+
+    @Override
+    public void onStarted(AbstractBuild build, TaskListener listener) {
+        WebHookPublisher publisher = GetWebHookPublisher(build);
+        if (publisher == null || !publisher.onStart) {
+            return;
+        }
+        String webHookUrl = publisher.webHookUrl;
+        String buildUrl = build.getAbsoluteUrl();
+        String projectName = build.getProject().getDisplayName();
+        String buildName = build.getDisplayName();
+        String buildVars = build.getBuildVariables().toString();
+        NotificationEvent event = new NotificationEvent(projectName, buildName, buildUrl, buildVars, "start");
+        httpPost(webHookUrl, event);
+    }
+
+    @Override
+    public void onCompleted(AbstractBuild build, @Nonnull TaskListener listener) {
+        WebHookPublisher publisher = GetWebHookPublisher(build);
+        if (publisher == null) {
+            return;
+        }
+        Result result = build.getResult();
+        if (result == null) {
+            return;
+        }
+        String webHookUrl = publisher.webHookUrl;
+        String buildUrl = build.getAbsoluteUrl();
+        String projectName = build.getProject().getDisplayName();
+        String buildName = build.getDisplayName();
+        String buildVars = build.getBuildVariables().toString();
+        NotificationEvent event = new NotificationEvent(projectName, buildName, buildUrl, buildVars, "");
+        if (publisher.onSuccess && result.equals(Result.SUCCESS)) {
+            event.event = "success";
+            httpPost(webHookUrl, event);
+        }
+        if (publisher.onFailure && result.equals(Result.FAILURE)) {
+            event.event = "failure";
+            httpPost(webHookUrl, event);
+        }
+        if (publisher.onUnstable && result.equals(Result.UNSTABLE)) {
+            event.event = "unstable";
+            httpPost(webHookUrl, event);
+        }
+    }
+
+    private WebHookPublisher GetWebHookPublisher(AbstractBuild build) {
+        for (Object publisher : build.getProject().getPublishersList().toMap().values()) {
+            if (publisher instanceof WebHookPublisher) {
+                return (WebHookPublisher) publisher;
+            }
+        }
+        return null;
+    }
+
+    private void httpPost(String url, Object object) {
+        String jsonString = JSON.toJSONString(object);
+        RequestBody body = RequestBody.create(JSON_MEDIA_TYPE, jsonString);
+        Request request = new Request.Builder().url(url).post(body).build();
+        try {
+            Response response = client.newCall(request).execute();
+            log.debug("Invocation of webhook {} successful", url);
+        } catch (Exception e) {
+            log.info("Invocation of webhook {} failed", url, e);
+        }
+    }
+}
---
+++ src/main/java/io/jenkins/plugins/WebHookPublisher.java
@@ -0,0 +1,61 @@
+package io.jenkins.plugins;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import hudson.tasks.Notifier;
+import hudson.tasks.BuildStepMonitor;
+import hudson.Extension;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Publisher;
+import hudson.model.AbstractProject;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import java.io.IOException;
+
+public class WebHookPublisher extends Notifier {
+    public String webHookUrl;
+    public Boolean onStart;
+    public Boolean onSuccess;
+    public Boolean onFailure;
+    public Boolean onUnstable;
+
+    @DataBoundConstructor
+    public WebHookPublisher(String webHookUrl, boolean onStart, boolean onSuccess, boolean onFailure, boolean onUnstable) {
+        super();
+        this.webHookUrl = webHookUrl;
+        this.onStart = onStart;
+        this.onSuccess = onSuccess;
+        this.onFailure = onFailure;
+        this.onUnstable = onUnstable;
+    }
+
+    @Override
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+            throws InterruptedException, IOException {
+        return true;
+    }
+
+    @Override
+    public WebHookPublisherDescriptor getDescriptor() {
+        return (WebHookPublisherDescriptor) super.getDescriptor();
+    }
+
+    @Extension
+    public static class WebHookPublisherDescriptor extends BuildStepDescriptor<Publisher> {
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "OpsLevel Integration";
+        }
+    }
+}
